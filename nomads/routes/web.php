ed507a33d63e6827f7cfa1b6175fade10f871752
yahyaabdurr <yahya.abdurr@gmail.com>
2020-05-04 00:31:47 +0700

    integrasi laravel dan layout

--- nomads/routes/web.php
+++ nomads/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::prefix('admin')
+    ->namespace('Admin')
+    ->group(function(){
+        Route::get('/','DashboardController@index')
+            ->name('dashboard');
+    });
